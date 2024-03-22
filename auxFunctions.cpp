f220ae0384a7d1bebaf650afab214e3082326966
Diego <amorenadiegog@gmail.com>
2024-03-22 11:01:41 -0300

    results

--- auxFunctions.cpp
+++
@@ -1,9 +0,0 @@
-#include "auxFunctions.h"
-
-std::chrono::duration<double>::rep Time(std::function<void()> func) {
-    auto start = std::chrono::high_resolution_clock::now();
-    func();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end - start;
-    return diff.count();
-}
