cbd4ce297b391fac9fe5203c6299dce51bdd06d9
DiegoAmorena <diegoamorenag@gmail.com>
2024-06-26 18:47:11 -0300

    asdsada

--- Practicos/Pr5/ej2/nuevo/main.cu
+++ Practicos/Pr5/ej2/nuevo/main.cu
@@ -50,11 +50,11 @@ struct TransformarNiveles {
     }
 };
 
-struct TransformarTamaño {
+struct TransformarTamaio {
     int* tamFila;
     int* orden;
 
-    TransformarTamaño(int* tamFila, int* orden) : tamFila(tamFila), orden(orden) {}
+    TransformarTamaio(int* tamFila, int* orden) : tamFila(tamFila), orden(orden) {}
 
     __host__ __device__ __forceinline__
     int operator()(const int &i) const {
@@ -242,8 +242,8 @@ int ordenar_filas(int* filaPtr, int* colIdx, int n, int* orden) {
     }
 
     int* ivect_size = (int*)malloc(n * sizeof(int));
-    TransformarTamaño transformarTamaño(filaPtr, orden);
-    thrust::transform(thrust::device, orden, orden + n, ivect_size, transformarTamaño);
+    TransformarTamaio transformarTamaño(filaPtr, orden);
+    thrust::transform(thrust::device, orden, orden + n, ivect_size, transformarTamaio);
 
     int* ivectsAux = new int[n * sizeof(int)];
     thrust::copy(ivects, ivects + num_levels, ivectsAux);
