fb2a33f3ca03653483b59270209f0c2a8760c878
DiegoAmorena <diegoamorenag@gmail.com>
2024-06-26 19:07:22 -0300

    asddsa

--- Practicos/Pr5/ej2/nuevo/main.cu
+++ Practicos/Pr5/ej2/nuevo/main.cu
@@ -581,8 +581,7 @@ int main(int argc, char** argv)
 
     int * iorder  = (int *) calloc(n,sizeof(int));
 
-    int nwarps = ordenar_filas(RowPtrL_d, ColIdxL_d, Val_d, n, iorder, extraParam);
-
+    int nwarps = ordenar_filas(RowPtrL_d, ColIdxL_d, Val_d, n, iorder);
     printf("Number of warps: %i\n",nwarps);
     for(int i =0; i<n && i<20;i++)
         printf("Iorder[%i] = %i\n",i,iorder[i]);
