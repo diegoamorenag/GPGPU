fba0dee90cdd071c783e1106c1206066493db5bc
DiegoAmorena <diegoamorenag@gmail.com>
2024-06-27 12:33:52 -0300

    sadas

--- Practicos/Pr5/ej2/ejecutable.sh
+++ Practicos/Pr5/ej2/ejecutable.sh
@@ -1,16 +1,16 @@
 cd a/
 make
-./biblios ../matrices/A_matrix.mtx
-./biblios ../matrices/1.mtx
-./biblios ../matrices/2.mtx
-./biblios ../matrices/3.mtx
-./biblios ../matrices/4.mtx
-./biblios ../matrices/5.mtx
-cd ellos/
+cd ../ellos/
 make
-./biblios ../matrices/A_matrix.mtx
-./biblios ../matrices/1.mtx
-./biblios ../matrices/2.mtx
-./biblios ../matrices/3.mtx
-./biblios ../matrices/4.mtx
-./biblios ../matrices/5.mtxbi
+./a/biblios ../matrices/A_matrix.mtx
+./ellos/biblios ../matrices/A_matrix.mtx
+./a/biblios ../matrices/1.mtx
+./ellos/biblios ../matrices/1.mtx
+./a/biblios ../matrices/2.mtx
+./ellos/biblios ../matrices/2.mtx
+./a/biblios ../matrices/3.mtx
+./ellos/biblios ../matrices/3.mtx
+./a/biblios ../matrices/4.mtx
+./ellos/biblios ../matrices/4.mtx
+./a/biblios ../matrices/5.mtx
+./ellos/biblios ../matrices/5.mtxbi
