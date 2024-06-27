6b8e26b4791c0a6ae61da702b681645d20fc058e
DiegoAmorena <diegoamorenag@gmail.com>
2024-06-27 12:58:40 -0300

    ejecutable

--- Practicos/Pr5/ej2/ejecutable.sh
+++ Practicos/Pr5/ej2/ejecutable.sh
@@ -1,14 +1,20 @@
+mkdir -p ../results # Ensure the results directory exists
+result_file="../results/all_results.txt" # Define the result file path
+
+# Clear the result file at the start or create it if it doesn't exist
+: > "$result_file"
+
 cd a/ && make
 cd ../ellos/ && make
-cd ../a/ && ./biblios ../matrices/A_matrix.mtx
-cd ../ellos/ && ./biblios ../matrices/A_matrix.mtx
-cd ../a/ && ./biblios ../matrices/1.mtx
-cd ../ellos/ && ./biblios ../matrices/1.mtx
-cd ../a/ && ./biblios ../matrices/2.mtx
-cd ../ellos/ && ./biblios ../matrices/2.mtx
-cd ../a/ && ./biblios ../matrices/3.mtx
-cd ../ellos/ && ./biblios ../matrices/3.mtx
-cd ../a/ && ./biblios ../matrices/4.mtx
-cd ../ellos/ && ./biblios ../matrices/4.mtx
-cd ../a/ && ./biblios ../matrices/5.mtx
-cd ../ellos/ && ./biblios ../matrices/5.mtx
+cd ../a/ && ./biblios ../matrices/A_matrix.mtx >> "$result_file"
+cd ../ellos/ && ./biblios ../matrices/A_matrix.mtx >> "$result_file"
+cd ../a/ && ./biblios ../matrices/1.mtx >> "$result_file"
+cd ../ellos/ && ./biblios ../matrices/1.mtx >> "$result_file"
+cd ../a/ && ./biblios ../matrices/2.mtx >> "$result_file"
+cd ../ellos/ && ./biblios ../matrices/2.mtx >> "$result_file"
+cd ../a/ && ./biblios ../matrices/3.mtx >> "$result_file"
+cd ../ellos/ && ./biblios ../matrices/3.mtx >> "$result_file"
+cd ../a/ && ./biblios ../matrices/4.mtx >> "$result_file"
+cd ../ellos/ && ./biblios ../matrices/4.mtx >> "$result_file"
+cd ../a/ && ./biblios ../matrices/5.mtx >> "$result_file"
+cd ../ellos/ && ./biblios ../matrices/5.mtx >> "$result_file"
