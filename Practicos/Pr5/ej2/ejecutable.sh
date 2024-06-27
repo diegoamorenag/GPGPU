9725549cd7ef9b1c1415b2a06977ea7e927ebd90
DiegoAmorena <diegoamorenag@gmail.com>
2024-06-27 12:26:19 -0300

    ejecutable

---
+++ Practicos/Pr5/ej2/ejecutable.sh
@@ -0,0 +1,14 @@
+cd /a/
+make
+./biblios matrices\A_matrix.mtx
+./biblios matrices\Andrews.mtx
+./biblios matrices\PGPgiantcompo_coord.mtx
+./biblios matrices\soc-sign-bitcoin-alpha_nodeid.mtx
+./biblios matrices\sx-mathoverflow_A2Q.mtx
+cd /OG/
+make
+./biblios matrices\A_matrix.mtx
+./biblios matrices\Andrews.mtx
+./biblios matrices\PGPgiantcompo_coord.mtx
+./biblios matrices\soc-sign-bitcoin-alpha_nodeid.mtx
+./biblios matrices\sx-mathoverflow_A2Q.mtx
