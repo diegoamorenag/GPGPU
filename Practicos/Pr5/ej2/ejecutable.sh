56438f77cf9ef9981fd9874987ff965a29507ebb
DiegoAmorena <diegoamorenag@gmail.com>
2024-06-27 12:38:22 -0300

    asdas

--- Practicos/Pr5/ej2/ejecutable.sh
+++ Practicos/Pr5/ej2/ejecutable.sh
@@ -1,16 +1,14 @@
-cd a/
-make
-cd ../ellos/
-make
-./a/biblios ../matrices/A_matrix.mtx
-./ellos/biblios ../matrices/A_matrix.mtx
-./a/biblios ../matrices/1.mtx
-./ellos/biblios ../matrices/1.mtx
-./a/biblios ../matrices/2.mtx
-./ellos/biblios ../matrices/2.mtx
-./a/biblios ../matrices/3.mtx
-./ellos/biblios ../matrices/3.mtx
-./a/biblios ../matrices/4.mtx
-./ellos/biblios ../matrices/4.mtx
-./a/biblios ../matrices/5.mtx
-./ellos/biblios ../matrices/5.mtxbi
+cd a/ && make
+cd ../ellos/ && make
+cd ../a/ && ./biblios ../matrices/A_matrix.mtx
+cd ../ellos/ && ./biblios ../matrices/A_matrix.mtx
+cd ../a/ && ./biblios ../matrices/1.mtx
+cd ../ellos/ && ./biblios ../matrices/1.mtx
+cd ../a/ && ./biblios ../matrices/2.mtx
+cd ../ellos/ && ./biblios ../matrices/2.mtx
+cd ../a/ && ./biblios ../matrices/3.mtx
+cd ../ellos/ && ./biblios ../matrices/3.mtx
+cd ../a/ && ./biblios ../matrices/4.mtx
+cd ../ellos/ && ./biblios ../matrices/4.mtx
+cd ../a/ && ./biblios ../matrices/5.mtx
+cd ../ellos/ && ./biblios ../matrices/5.mtx
