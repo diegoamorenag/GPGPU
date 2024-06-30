8e387bbdeee74d3bec2e4915903023d892b50c84
DiegoAmorena <diegoamorenag@gmail.com>
2024-06-29 21:20:12 -0300

    sfdsfs

--- Practicos/labfinal/radixSort.h
+++ Practicos/labfinal/radixSort.h
@@ -2,12 +2,12 @@
 #ifndef RADIX_SPRT_H
 #define RADIX_SPRT_H
 
-void exclusiveScanCPU(/*const*/ float *input, float *output, int numElements);
-void splitCPU(/*const*/ float *input, float *output, int n, int numElements) ;
-void radixSortCPU(/*const*/ float *input, float *output, int numElements);
+void exclusiveScanCPU(const float *input, float *output, int numElements);
+void splitCPU(const float *input, float *output, int n, int numElements) ;
+void radixSortCPU(const float *input, float *output, int numElements);
 
-void exclusiveScanGPU(/*const*/ float *input, float *output, int numElements);
-void splitCPU(/*const*/ float *input, float *output, int n, int numElements) ;
-void radixSortGPU(/*const*/ float *input, float *output, int numElements);
+void exclusiveScanGPU(const float *input, float *output, int numElements);
+void splitGPU(const float *input, float *output, int n, int numElements) ;
+void radixSortGPU(const float *input, float *output, int numElements);
 
 #endif // RADIX_SPRT_H
